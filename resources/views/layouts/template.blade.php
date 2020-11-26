b9cf5b7258a4358153902204700d76ba279ff025
Tama <pratama.nugraha@binus.ac.id>
2020-11-26 17:34:29 +0700

    benerin dropdown

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>$okopedia</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('assets/js/jquery.min.js') }}" defer></script>
-    <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}" defer></script>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
